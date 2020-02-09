7db73288071b70a13f0870b3a1ec1e1a210379ba
grantholly <gdholly@gmail.com>
2020-02-09 11:36:21 -0800

    adding supporting shell scripts for quick startup

---
+++ run_redis.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+docker run -d \
+       --name python_redis_server \
+       redis:5.0.7
---
+++ watch_redis.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+redis-cli \
+    -h 127.0.0.1 \
+    monitor
